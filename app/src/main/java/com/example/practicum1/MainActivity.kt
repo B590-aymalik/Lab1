d2c9df2dbaf98e093e87d4c153a2421735ee50c5
ayushm98 <ayushkumarmalik10@gmail.com>
2025-02-13 16:32:39 -0600

    made some final changes

--- app/src/main/java/com/example/practicum1/MainActivity.kt
+++ app/src/main/java/com/example/practicum1/MainActivity.kt
@@ -1,28 +1,37 @@
 package com.example.practicum1
-
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import android.widget.Button
-import android.view.View
+import androidx.constraintlayout.utils.widget.MotionButton
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
         trueButton = findViewById(R.id.true_button)
-        falseButton = findViewById(R.id.false_button)
+        falseButton = findViewById((R.id.false_button))
 
-        trueButton.setOnClickListener{
-            view : View ->
+        trueButton.setOnClickListener {
+                view: View->
+            Toast.makeText(
+                this,
+                R.string.correctToast,
+                Toast.LENGTH_SHORT
+            ).show()
         }
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
     }
 }
